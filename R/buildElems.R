fedabfacdea9ab812c0f024b1221cafa48369ea7
Jinhwan Kim <hwanistic@gmail.com>
2023-09-26 02:30:02 +0000

    version 1.0.0

--- R/buildElems.R
+++ R/buildElems.R
@@ -10,30 +10,19 @@
 #' @export
 #'
 #' @examples
-#' nodes = buildElems( # will generate 5 nodes
-#'  elems = data.frame(
-#'    id = paste0('node',1:5),
-#'    bgColor = "#FFFFFF",
-#'    borderColor = "#48DBFB",
-#'    borderWidth = 2,
-#'    fontSize = 10,
-#'    width = 60, height = 20, opacity = 1, stringsAsFactors = FALSE
-#'  ), type = "Node")
-#'
+#' nodes <- buildElems( # will generate 5 nodes
+#'   elems = data.frame(
+#'     id = paste0("node", 1:5),
+#'     bgColor = "#FFFFFF",
+#'     borderColor = "#48DBFB",
+#'     borderWidth = 2,
+#'     fontSize = 10,
+#'     width = 60, height = 20, opacity = 1, stringsAsFactors = FALSE
+#'   ), type = "Node"
+#' )
 #'
 buildElems <- function(elems, type) {
-  func <- paste0("build", type, "(")
-
-  res <- list()
-  params <- colnames(elems)
-  for (i in 1:nrow(elems)) {
-    command <- c()
-    for (j in 1:length(params)) {
-      command[j] <- paste0(params[j], " = ", "'", elems[i, j], "'")
-    }
-    command <- paste(command, collapse = ", ")
-    command <- paste0(func, command, ")")
-    res[[length(res) + 1]] <- eval(parse(text = command))
-  }
+  func <- paste0("build", type)
+  res <- lapply(seq_len(nrow(elems)), function(i) do.call(func, as.list(elems[i, ])))
   return(res)
 }
